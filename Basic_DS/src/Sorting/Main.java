bf155612229d58ea0e0976a432346faf566b902c
Mayur Kapurkar <mayurkapurkar@gmail.com>
2022-02-19 22:17:20 +0530

    changes in main methods

--- Basic_DS/src/Sorting/Main.java
+++ Basic_DS/src/Sorting/Main.java
@@ -3,11 +3,12 @@ package Sorting;
 public class Main {
     public static void main(String[] args) throws Exception {
         Bubble<Integer> bubble = new Bubble<Integer>();
+        MergeSort<Integer> mergeSort = new MergeSort<>();
         // Selection<Integer> selection = new Selection<>();
         // Insertion<Integer> insertion = new Insertion<>();
         Integer[] arr = { 10, 90, 30, 15, 60, 80, 45 };
 
-        Integer[] arr2 = bubble.sort(arr);
+        Integer[] arr2 = mergeSort.sort(arr);
         // Integer[] arr2 = Bubble.sortModified(arr);
         // Integer[] arr2 = selection.sort(arr);
         for (Integer integer : arr2) {
